b834285a7d3e6cb5b89cd07d4d2f5f40add6466c
Juan Rivera <indiojuan2012.jr@gmail.com>
2023-07-26 16:10:31 -0300

    scroll nav

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -8,14 +8,27 @@ import { Link as Anchor, useNavigate, useLocation } from "react-router-dom";
 export default function Navbar() {
 
     const [isOpen, setIsOpen] = useState(false)
+    const [scrolled, setScrolled] = useState(false);
 
-
-
-
+    // Cambio de color en navbar al hacer scroll
+    const handleScroll = () => {
+        const offset = window.scrollY;
+        if (offset > 130) {
+            setScrolled(true);
+        } else {
+            setScrolled(false);
+        }
+    };
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
 
     return (
-        <header>
-            <nav className="navbar">
+        <header className={scrolled ? "navbar scrolled " : "navbar"}>
+            <nav >
 
                 <div className='logo'>
                     <Anchor to={`/`} >Tease. <span>IT</span></Anchor>
