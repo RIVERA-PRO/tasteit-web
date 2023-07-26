0af8ab327baca4ec496604c0d9f6d72a4855809d
Juan Rivera <indiojuan2012.jr@gmail.com>
2023-07-26 16:19:41 -0300

    btn scroll + responsive nav

--- src/Layouts/IndexLayout.jsx
+++ src/Layouts/IndexLayout.jsx
@@ -3,6 +3,7 @@ import Header from '../Pages/Header/Header'
 import Footer from '../Components/Footer/Footer'
 import Developer from '../Components/Developer/Developer'
 import Hero from '../Components/Hero/Hero'
+import ButonScroll from '../Components/ButonScroll/ButonScroll'
 export default function IndexLayout() {
     return (
         <div>
@@ -10,6 +11,7 @@ export default function IndexLayout() {
             <Hero />
             <Footer />
             <Developer />
+            <ButonScroll />
         </div>
     )
 }
